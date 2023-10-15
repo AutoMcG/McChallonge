0a6de00ee6cd4188a82f8b283677bee1f9f38150
AutoMcG <aaronmcguiretech@gmail.com>
2023-10-15 15:15:31 -0600

    Simplified player win/loss count

--- src/services/think.py
+++ src/services/think.py
@@ -17,8 +17,6 @@ def count_outcomes(matches: [Match], pilots: [Pilot]) -> [Pilot]:
         loser = match.loser_id
         pilot_winner = next((pilot for pilot in pilot_outcomes if pilot.id == winner), None)
         pilot_loser = next((pilot for pilot in pilot_outcomes if pilot.id == loser), None)
-        pilot_winner.wins = 1 if not (pilot_winner and hasattr(pilot_winner, "wins")) else pilot_winner.wins + 1
-        pilot_loser.losses = 1 if not (pilot_loser and hasattr(pilot_loser, "losses")) else pilot_loser.losses + 1
-#       if pilot_winner and hasattr(pilot_winner, "wins") : pilot_winner.wins += 1 else pilot_winner.wins = 1
-#       if pilot_loser : pilot_loser.losses += 1
+        pilot_winner.wins += 1
+        pilot_loser.losses += 1
     return pilot_outcomes
