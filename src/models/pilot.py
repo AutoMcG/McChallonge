c4c572652681944b650fe76ca04aef86bf5a43aa
AutoMcG <aaronmcguiretech@gmail.com>
2023-10-14 18:55:24 -0600

    Models now swap "empty" for nulls on parse

--- src/models/pilot.py
+++ src/models/pilot.py
@@ -4,7 +4,8 @@ import pprint
 
 class Pilot(object):
     def __init__(self, j):
-        self.__dict__ = json.loads(j)
+        none_to_empty = j.replace('null', '"empty"')
+        self.__dict__ = json.loads(none_to_empty)
     def __str__(self):
         pp = pprint.PrettyPrinter()
         return pp.pformat(self.__dict__)
